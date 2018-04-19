db51fa2c227984a28b5e9e61eea4fadcb8d58f92
Philipp Mainz <pmainz@varion.de>
2018-04-19 13:36:13 +0200

    use search path only once

--- settings.js
+++ settings.js
@@ -1,21 +1,24 @@
 const q = require('q');
 const fs = require('fs');
 const whereis = require("node-whereis");
+const path require('./path')
 
 module.exports.storePath = function () {
   var defer = q.defer();
-  var t = {
-    mongodump: whereis('mongodump'),
-    mongorestore:whereis('mongorestore')
-  };
-  
-  
-  fs.writeFile(__dirname+'/path.js', `
+  if (path.first) {
+    var t = {
+      mongodump: whereis('mongodump'),
+      mongorestore: whereis('mongorestore')
+    };
+
+
+    fs.writeFile(__dirname + '/path.js', `
 module.exports.first = false;
 module.exports.mongodumpPath = '${t.mongodump}';
 module.exports.mongorestorePath = '${t.mongorestore}';
-`, (err) => {  
-    defer.resolve();
-  });
+`, (err) => {
+      defer.resolve();
+    });
+  } else defer.resovle();
   return defer.promise;
 }
